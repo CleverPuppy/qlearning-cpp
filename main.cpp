a77a392313f7e11c545d4dab5bbe2df5a65c1701
dph <919479850@qq.com>
2020-09-18 16:57:23 +0800

    增加Loss统计

--- main.cpp
+++ main.cpp
@@ -6,20 +6,27 @@ using namespace std;
 
 void testTreasureHunt(std::string filename)
 {
-    float learning_rate = 0.01;
+    float learning_rate = 1.0f;
     float gamma = 0.8;
-    float epsilon = 0.9;
+    float epsilon = 1.0;
     TreasureHuntQL treasureHuntQL(filename,
                                   learning_rate,
                                   epsilon,
                                   gamma);
+    std::cout << "#Params "
+              << " learning_rate : " << learning_rate
+              << " gamma : " << gamma 
+              << " epsilon : " << epsilon
+              << std::endl;
+    std::cout << "#Episode Steps AverageLoss" << std::endl;
     for(int i = 0; i < 1000; ++i){
+        std::cout << i << " ";
         treasureHuntQL.run(treasureHuntQL.start_state);
     }
 }
 
 int main()
 {
-    testTreasureHunt("tests/treasurehunt_map.txt");
+    testTreasureHunt("../tests/treasurehunt_map.txt");
     return 0;
 }
